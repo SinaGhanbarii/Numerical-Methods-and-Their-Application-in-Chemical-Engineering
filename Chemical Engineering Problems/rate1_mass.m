af585451c976ebc4117beff5fff89b1ceae3f160
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-01-29 16:31:49 +0100

    Edit

---
+++ Chemical Engineering Problems/rate1_mass.m
@@ -0,0 +1,15 @@
+% rate1_mass.m
+function ydot=rate1_mass(VR,y)
+global k vel km
+% y(1) is CA, y(2) is CB, y(3) is CC
+CA=y(1);
+% mass transfer problem
+OPTIONS=[]
+CAguess=CA;
+CAs=fzero(@mass_rxn,CAguess,OPTIONS,CA);
+rate=k*CAs*CAs;
+ydot(1)=-2.*rate/vel;
+ydot(2)=+rate/vel;
+ydot(3)=0.;
+ydot=ydot';
+end
